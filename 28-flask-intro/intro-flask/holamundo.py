66044f94c25b12be73845afcc64c604019c0f258
José Bello <jfbello.work@gmail.com>
2024-10-24 18:53:43 +0200

    28-06 Flask: Datos de formulario

--- 28-flask-intro/intro-flask/holamundo.py
+++ 28-flask-intro/intro-flask/holamundo.py
@@ -14,7 +14,13 @@ def lala(post_id):
     else:
         return 'El método NO ES GET'
 
+# La llamada a curl:
+# 	curl -d "llave1=dato1&llave2=dato2" -X POST http://localhost:5000/lele
 
-@app.route('/lele')
+
+@app.route('/lele', methods=['POST'])
 def lele():
+    print(request.form)
+    print(request.form['llave1'])
+    print(request.form['llave2'])
     return 'lele'
