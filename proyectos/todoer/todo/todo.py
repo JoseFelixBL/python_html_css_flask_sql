089dc6f8d2e927f5902b7d80426af096a6678857
José Bello <jfbello.work@gmail.com>
2024-10-28 13:32:29 +0100

    29-16 TODO: Actualizando el TODO

--- proyectos/todoer/todo/todo.py
+++ proyectos/todoer/todo/todo.py
@@ -46,14 +46,49 @@ def create():
     return render_template('todo/create.html')
 
 
+def get_todo(id):
+    db, c = get_db()
+    c.execute(
+        'select t.id, t.description, t.completed, t.created_by, t.created_at, u.username '
+        'from todo t join user u on t.created_by = u.id where t.id = %s',
+        (id,)
+    )
+
+    todo = c.fetchone()
+
+    if todo is None:
+        abort(404, f'El TODO de id {id} no existe')
+
+    return todo
+
+
 @bp.route('/<int:id>/update', methods=['GET', 'POST'])
 @login_required
 def update(id):
-    return render_template('todo/update.html', todo={
-        "description": "kk Mi todo",
-        "id": 2,
-        "completed": 0
-    })
+    todo = get_todo(id)
+
+    if request.method == 'POST':
+        description = request.form['description']
+        # Para el campo de tipo checkbox, completado, no sirve el request.form[...]
+        completed = True if request.form.get('completed') == 'on' else False
+        error = None
+
+        if not description:
+            error = "La Descripción es requerida."
+
+        if error is not None:
+            flash(error)
+        else:
+            db, c = get_db()
+            c.execute(
+                'update todo set description = %s, completed = %s'
+                ' where id = %s',
+                (description, completed, id)
+            )
+            db.commit()
+            return redirect(url_for('todo.index'))
+
+    return render_template('todo/update.html', todo=todo)
 
 
 @bp.route('/<int:id>/delete', methods=['POST'])
