8d5f540f44b5d0ff2bab28295e71c43c33d87d53
José Bello <jfbello.work@gmail.com>
2024-10-28 19:47:54 +0100

    30-08 Tk: Frames (no LabelFrames como antes) - sin texto ni bordes

--- 30-tkinter/tk/frame.py
+++ 30-tkinter/tk/frame.py
@@ -3,18 +3,14 @@ from tkinter import *
 root = Tk()
 root.title('Usando Frames')
 
-# Este padding es para dentro del marco
-frame = LabelFrame(root, text='Login', padx=10, pady=10, borderwidth=10)
-# Este padding es para afuera del marco
+# Los Frame no tiene texto ni marco, pero igualmente agrupan widgets
+frame = Frame(root, padx=10, pady=10, borderwidth=10)
 frame.pack(padx=50, pady=50)
 
-frame_sin = LabelFrame(root, padx=10, pady=10, borderwidth=10)
-frame_sin.pack(padx=50, pady=50)
 
 l = Label(frame, text='Estoy dentro de un Frame')
+btn = Button(frame, text='Salir', command=root.quit)
 l.pack()
-
-btn = Button(frame_sin, text='Salir', command=root.quit)
 btn.pack()
 
 root.mainloop()
