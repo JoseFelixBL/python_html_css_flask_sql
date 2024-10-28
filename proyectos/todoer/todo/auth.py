696fe6c6193c75c2f57bc04873fd2cea7565a463
José Bello <jfbello.work@gmail.com>
2024-10-28 10:41:44 +0100

    29-..12 proteger rutas, lista elementos, cerrar sesión, endpoints en blanco

--- proyectos/todoer/todo/auth.py
+++ proyectos/todoer/todo/auth.py
@@ -29,6 +29,8 @@ def register():
             error = f'Usuario {username} se encuentra registrado.'
             # error = 'Usuario {} se encuentra registrado.'.format(username)
         if error is None:
+            # error = f'{"+-" * 10}:  {len(generate_password_hash(password))}'
+            # flash(error)
             c.execute(
                 'insert into user (username, password) values (%s, %s)',
                 (username, generate_password_hash(password))
@@ -62,8 +64,39 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('todo.index'))
 
         flash(error)
 
     return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        db, c = get_db()
+        c.execute(
+            'select * from user where id = %s', (user_id,)
+        )
+        g.user = c.fetchone()
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+@bp.route('logout')
+def logout():
+    session.clear()
+    return redirect(url_for('auth.login'))
---
+++ proyectos/todoer/todo/todo.py
@@ -0,0 +1,33 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+# Si un usr trata de actualizar un todo que no le pertenezca, le mandamos abort
+from werkzeug.exceptions import abort
+from todo.auth import login_required
+from todo.db import get_db
+
+bp = Blueprint('todo', __name__)
+
+
+@bp.route('/')
+@login_required
+def index():
+    db, c = get_db()
+    c.execute(
+        'select t.id, t.description, u.username, t.completed, t.created_at from todo t JOIN user u on t.created_by = u.id order by created_at desc'
+    )
+    todos = c.fetchall()
+
+    return render_template('todo/index.html', todos=todos)
+
+
+@bp.route('/create', methods=['GET', 'POST'])
+@login_required
+def create():
+    return ''
+
+
+@bp.route('/update', methods=['GET', 'POST'])
+@login_required
+def update():
+    return ''
